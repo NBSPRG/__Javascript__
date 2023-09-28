c47e803f92afba754a159687e234c3d0329a4510
chandan kumar <116447157+NBSPRG@users.noreply.github.com>
2023-09-28 07:51:33 +0000

    datatype summary added

---
+++ Javascript/datatype_summary.js
@@ -0,0 +1,17 @@
+/*
+**Primitive DataTypes**
+    String  = string
+    Number = number
+    Decimal = number
+    Boolean = boolean 
+    null = object
+    undefined = undefined
+    Symbol = symbol
+    BIGInt = bigInt
+
+**Non-Primitive DataTypes**
+    Array = object
+    Function = function (But say it function object)
+    Object = object
+
+*/
