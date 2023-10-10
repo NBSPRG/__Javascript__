49a3b018c98727c89fd52f5950084e6e5d3eff77
chandan kumar <116447157+NBSPRG@users.noreply.github.com>
2023-10-10 18:54:03 +0000

    string basic added

---
+++ Javascript/string.js
@@ -0,0 +1,20 @@
+const naam = "Alice";
+const age = 30;
+
+const message = `My name is ${naam} and I am ${age} years old.`;
+
+console.log(message);
+
+const gameName = new String("Chandan%20Kumar")
+console.log(gameName.__proto__)
+
+// using different function in javascript
+console.log(gameName.length)
+console.log(gameName.charAt(2))
+console.log(gameName.indexOf('c'))
+
+const newString = (gameName.replace("%20", "-"))
+console.log(newString.split("-"))
+
+console.log(("      chandan      ").trimRight())
+console.log(("         chandan      ").trim())
